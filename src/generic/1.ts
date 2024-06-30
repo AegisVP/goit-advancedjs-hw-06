f7ee2b0b002063e0b8ecb9a3ea9bc868089ad3c9
Vlad P <vlad@pysarenko.com>
2024-06-30 02:47:23 +0200

    HW done

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,14 +3,15 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
-  return new Promise((resolve) => {
+type StringOrNumber = string | number;
+
+function getPromise(): Promise<StringOrNumber[]> {
+  return new Promise(resolve => {
     resolve(['Text', 50]);
   });
 }
 
-getPromise()
-.then((data) => {
+getPromise().then(data => {
   console.log(data);
 });
 
